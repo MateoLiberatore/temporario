0080171589eb426a5f63d2bf1dd09046d8f4b775
Mateo Liberatore <liberatoremate98@gmail.com>
2026-07-31 19:45:09 -0300

    Deploy: a09c457

---
+++ _next/static/chunks/01de962b54d2f2bf.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/7b926a71f02c2414.js",
+  "static/chunks/3fb99fb681d4ac3d.js",
+  "static/chunks/turbopack-c25259aa3d4e881e.js"
+])
---
+++ _next/static/chunks/a9265a60a0763b22.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/e376664127dd86b0.js",
+  "static/chunks/3fb99fb681d4ac3d.js",
+  "static/chunks/turbopack-7ba43557df6947e6.js"
+])
